4c871cc1c85d28d889da577f86205a21b4320c15
isEduardo13 <eduardoisidroh13@gmail.com>
2024-10-14 13:57:40 -0600

    Agrega nullable a propiedades en Models/Reporte.cs

--- Models/Reporte.cs
+++ Models/Reporte.cs
@@ -8,19 +8,19 @@ namespace ExamenUnidad2.Models
     public class Reporte
     {
          int id { get; set; }
-        private string nombreCliente { get; set; }
-        private string fallo { get; set; }
+        private string? nombreCliente { get; set; }
+        private string? fallo { get; set; }
         
-        private string estado{ get; set; }
+        private string? estado{ get; set; }
 
-        private string prioridad { get; set; }
+        private string? prioridad { get; set; }
 
-        public string fechaEnvio { get; set; }
+        public string? fechaEnvio { get; set; }
 
-        public string fechaRespuesta { get; set; }
+        public string? fechaRespuesta { get; set; }
 
-        public string fechaResolucion{ get; set; }
+        public string? fechaResolucion{ get; set; }
 
-        private string encargado { get; set; }
+        private string? encargado { get; set; }
     }
 }
