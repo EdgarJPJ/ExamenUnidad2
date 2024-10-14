07c5f6f5e38bc3b1486660eb0ad81f87c8d030dd
EdgarJPJ <edgarjaf1812@gmail.com>
2024-10-14 10:50:21 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExamenSegundaUnidad.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
